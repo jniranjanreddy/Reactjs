a1f46a400fb2735cff6ea196859c0cc2a39221ce
SG-Niranjan-Jannapureddy <you@example.com>
2022-06-07 03:58:21 -0400

    oi

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+const clickHandler = () => {
+  console.log("Clicked !!!!");
+  alert("Hey you clicked");
+};
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={clickHandler}>Click Me</button>
+    
   );
 }
 
-export default App;
+export default App
